9e240e3b773daad32fb06ed7e25483dbf2d11691
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-02-24 10:59:40 -0800

    new plots with unfiltered and unsampled data

--- py/desiforecast/query_database.py
+++ py/desiforecast/query_database.py
@@ -1,29 +1,37 @@
 """This script will query DESI telemetry databases and update any local pickled data. Tables and columns of interest are included below and can be modified to get additional data as needed.
 """
 
+import gc
+import os
 import psycopg2
 import pandas as pd
 import numba as nb
 import pickle
-import logging
 from tqdm import tqdm
 from datetime import datetime
 import pytz
+import argparse
+parser = argparse.ArgumentParser(
+                    prog = 'query_database',
+                    description = 'This program will submit SQL queries to get DESI telemetry data')
+parser.add_argument('-m', '--merge',
+                    action='store_true',
+                    help='merged and resampled data tables')
+args = parser.parse_args()
 
-def preprocess_data(df):
-    """Preprocesses DESI telemetry data contained within Pandas dataframe.
+def resample_data(df):
+    """Resamples DESI telemetry data contained within Pandas dataframe.
     
     Parameters
     ----------
     df : pandas.DataFrame
-        Unprocessed data as queried from the DESI telemetry database
+        Original data as queried from the DESI telemetry database
     
     Returns
     -------
     df : pandas.DataFrame
         Resampled and interpolated DESI telemetry data
     """
-    df.set_index('time_recorded', inplace=True)
     if 'seeing' in df.columns.values:
         pass
     else:
@@ -44,10 +52,9 @@ def main():
         None
     """
    
-    logging.basicConfig(filename=f'data/query.log')
-    logger = logging.getLogger()
-    logger.info(f'Query date: {datetime.now(pytz.timezone("America/Phoenix"))}')
-    logger.info(f'Preprocessed: {args.preprocess}\n')
+    directory = f'./data'
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
     
     # Data table names and respective columns of interest
     labels = {}
@@ -57,33 +64,38 @@ def main():
     labels['etc_seeing'] = ['time_recorded', 'etc_seeing', 'seeing']
     labels['etc_telemetry'] = ['time_recorded', 'seeing', 'transparency', 'skylevel']
     labels['tcs_info'] = ['time_recorded', 'mirror_ready', 'airmass']
-
-
+    
     # Establish connection to server and begin SQL queries using labels shown above.
-    try:
-        host = 'replicator-db.desi.lbl.gov'
-        database = 'desi_dev'
-        user = 'desi_reader'
-        password = 'reader'
-        for table, columns in tqdm(labels.items(), desc='Querying database table...'):
-            conn = psycopg2.connect(host=host, port='5432', database=database, user=user, password=password)
-            logger.info(f'{table}:')
-            with conn:
-                with conn.cursor() as cur:
-                    logger.info('\tQuerying database table...')
-                    cur.execute(f'SELECT {", ".join(columns)} FROM {table};')
-                    df = pd.DataFrame(cur.fetchall())
-                    df.columns = columns
-                    logger.info('\tPreprocessing data...')
-                    df = preprocess_data(df)
-                    logger.info('\tSaving to file...')
-                    with open(f'data/{table}.pkl', 'wb') as pf:
+    host = 'replicator-db.desi.lbl.gov'
+    port = '5432'
+    database = 'desi_dev'
+    user = 'desi_reader'
+    password = 'reader'
+    first = True
+    data = pd.DataFrame()
+    for table, columns in tqdm(labels.items(), desc='Querying database tables...'):
+        conn = psycopg2.connect(host=host, port=port, database=database, user=user, password=password)
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(f'SELECT {", ".join(columns)} FROM {table};')
+                df = pd.DataFrame(cur.fetchall())
+                df.columns = columns
+                df.set_index('time_recorded', inplace=True)
+                df.sort_index(inplace=True)
+                if args.merge:
+                    df = resample_data(df)
+                    if first:
+                        data = df
+                    else:
+                        data = data.join(df)
+                else:
+                    with open(f'{directory}/{table}.pkl', 'wb') as pf:
                         pickle.dump(df, pf)
-                    logger.info('\tQuery complete!')
-    except Exception as e:
-        logger.error(f'\nQuery failed!\n')
-        logger.exception(f'{e}\n')
-
+                print(f'Queried {table}!')
+        first = False
+    if args.merge:
+        with open(f'{directory}/desi_telemetry_data.pkl', 'wb') as pf:
+            pickle.dump(df, pf)
 
 if __name__ == '__main__':
     main()
